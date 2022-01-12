7d95a0278477be083cc8036f7675709a1d87e5e8
Vrezerino <parkpatrick1@gmail.com>
2022-01-12 23:50:36 +0200

    code unique player list and game statistics functions

--- src/services/rpsService.js
+++ src/services/rpsService.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 import { baseUrl } from '../utils/utils';
 
 let i = 0;
+// Get game history data using cursors.
 export const getHistory = async (cursor, data = []) => {
 	const response = await axios.get(`${baseUrl}/history${cursor ? `?cursor=${cursor}` : ''}`);
-	console.log(response.data.cursor);
 	const currentCursor = response.data.cursor.split('=')[1];
 	i++;
-	if (i === 5) // Get five pages of data
+
+	if (i === 5) // Get five pages of data.
 		return data;
+
 	data.push(...response.data.data);
 	return getHistory(currentCursor, data);
 };
